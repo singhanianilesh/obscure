f17aeb93e34db0a82fdddd997cdc42932e2670d8
Singhania,Nilesh <Nilesh.Singhania@Cerner.com>
2015-10-06 09:37:31 -0500

    Simple java obfuscated code

---
+++ Main.java
@@ -0,0 +1,4 @@
+/*go/*$$_/^\u002A\u002Fclass Main{public static
+void main(String... $$){\u0066or(\u0063har $:
+"ÙóëÙóOëÝáëÙÙóáëÝ"\u002EtoCharArray())\u0053ystem
+\u002Eout\u002Eprint((char) (($ - 15) >> 1));}}
